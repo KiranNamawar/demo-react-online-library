9f89c4c8c588543d49df4512c8d54c5d6344dfb1
Kiran Namawar <kirannamawar@gmail.com>
2025-11-20 10:54:13 +0530

    install and configure react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Outlet } from "react-router";
+import { Library } from "lucide-react";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Online Library</h1>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <div className="text-3xl flex items-center gap-1">
+          <Library size={30} />
+          <h1>Online Library</h1>
+        </div>
+      </header>
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <footer>Footer</footer>
+    </div>
   );
 }
 
