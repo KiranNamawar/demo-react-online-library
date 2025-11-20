4cbfcdcc81038265298874792445de84350b0f7b
Kiran Namawar <kirannamawar@gmail.com>
2025-11-20 10:25:39 +0530

    install and integrate tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-
   return (
     <>
-      
+      <h1 className="text-3xl font-bold underline">Online Library</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
