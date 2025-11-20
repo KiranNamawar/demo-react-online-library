5bd6c6c324fd2ae8a7a4680eef455fcf5bcd8289
Kiran Namawar <kirannamawar@gmail.com>
2025-11-20 23:10:42 +0530

    add route configs for browse, book detail, new book, error pages

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
 import Home from "./routes/home";
+import Browse from "./routes/browse";
+import Category from "./routes/category";
+import NewBook from "./routes/new-book";
+import BookDetail from "./routes/book-detail";
+import ErrorPage from "./routes/error";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +15,34 @@ const router = createBrowserRouter([
         index: true,
         Component: Home,
       },
+      {
+        path: "browse",
+        children: [
+          {
+            index: true,
+            Component: Browse,
+          },
+          {
+            path: ":category",
+            Component: Category,
+          },
+        ],
+      },
+      {
+        path: "book",
+        children: [
+          {
+            index: true,
+            Component: NewBook,
+          },
+          {
+            path: ":bookId",
+            Component: BookDetail,
+          },
+        ],
+      },
     ],
+    ErrorBoundary: ErrorPage,
   },
 ]);
 
